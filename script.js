74ed3923f666ac5635ad60255e06e1a0af9e4fe9
mmewery <78642044+mmewery@users.noreply.github.com>
2021-02-06 13:17:02 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+
+let width=window.innerWidth -100;
+let height=window.innerHeight -50;
+function run(){
+	let top=Math.random()*height;
+	let left=Math.random()*width;
+	document.getElementById('ya').style.top=top+"px";
+	document.getElementById('ya').style.left=left+"px";
+}
+function answerOk(){
+		alert("asbgfbhanh");
+}
+function answerNo(){
+		alert("hdhngn");
+}
+
